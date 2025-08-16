35a6abbf4919e6f87f98aedc3a882ce7f9a60799
blitzdawolf <BlitzDaWolf>
2025-08-16 16:59:37 +0200

    init

---
+++ TOP_Utils/ActivityExtention.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+namespace TOP_Utils
+{
+    public static class ActivityExtention
+    {
+        private static string BaseName = string.Empty;
+        private static Dictionary<string, ActivitySource> activitySources = new Dictionary<string, ActivitySource>();
+
+        public static void SetBaseName(string name)
+        {
+            BaseName = name;
+        }
+
+        public static Activity? StartActivity(this object o, string activityName, ActivityKind kind = ActivityKind.Internal, string? parentID = null)
+        {
+            var fillname = BaseName + (string.IsNullOrEmpty(BaseName) ? "" : ".") + o.GetType().FullName;
+            if (!activitySources.ContainsKey(fillname))
+                activitySources.Add(fillname, new ActivitySource(fillname));
+            return activitySources[fillname].StartActivity(activityName, kind, parentID);
+        }
+
+        public static Activity? StartActivity<T>(string activityName, ActivityKind kind = ActivityKind.Internal, string? parentID = null)
+        {
+            var fillname = BaseName + (string.IsNullOrEmpty(BaseName) ? "" : ".") + typeof(T).FullName;
+            if (!activitySources.ContainsKey(fillname))
+                activitySources.Add(fillname, new ActivitySource(fillname));
+            return activitySources[fillname].StartActivity(activityName, kind, parentID);
+        }
+    }
+}
